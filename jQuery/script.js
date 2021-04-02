c3cb8b218023ae3c53f323bf61c30dbfe37ceeb3
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-02 08:23:52 +0200

    testing

--- jQuery/script.js
+++ jQuery/script.js
@@ -1,1 +1,13 @@
 "use_strict";
+
+let hide = false;
+
+$("#hide").click(function () {
+  if (!hide) {
+    $(".example").hide();
+    hide = true;
+  } else {
+    $(".example").show();
+    hide = false;
+  }
+});
