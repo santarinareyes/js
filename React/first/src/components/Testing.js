6a3f4f58a46189f4871e153312867d03a08ea530
Richard <richard.santarinareyes@medieinstitutet.se>
2021-04-12 14:53:56 +0200

    testing

--- React/first/src/components/Testing.js
+++ React/first/src/components/Testing.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // const Testing = (props) => {
 //   return (
@@ -9,15 +9,35 @@ import React from "react";
 //   );
 // };
 
-class Testing extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Testing: {this.props.name}</h1>
-        <h1>Testing: {this.props.testProp}</h1>
-      </div>
-    );
-  }
-}
+// class Testing extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Testing: {this.props.name}</h1>
+//         <h1>Testing: {this.props.testProp}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+const Testing = () => {
+  let [count, setCount] = useState(0);
+
+  const plus = () => {
+    setCount(count++);
+  };
+
+  const minus = () => {
+    setCount(count--);
+  };
+
+  return (
+    <div className="Testing">
+      <p>{count}</p>
+      <button onClick={plus}>Plus</button>
+      <button onClick={minus}>Minus</button>
+    </div>
+  );
+};
 
 export default Testing;
